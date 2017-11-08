228a404e421d9b10e780451f3b54cefe03f3fe0e
Ralf Oenning <ralf@invicto.net>
2017-11-08 13:01:33 +0000

    finding line breaks multiline mode

---
+++ src/test/scala/example/LineBreaksTest.scala
@@ -0,0 +1,39 @@
+package example
+
+import org.scalatest.{FlatSpec, Matchers}
+
+/**
+  * https://stackoverflow.com/questions/1171284/regex-to-match-eof
+  */
+
+class LineBreaksTest extends FlatSpec with Matchers {
+
+  "finding line breaks" should "work" in {
+
+    //https://stackoverflow.com/questions/454908/split-java-string-by-new-line
+    val multilineString =
+      """
+
+    """
+    println("""[\r\n]+""".r.findAllIn(multilineString).toList)    //one or more of \r or \n so matches \r, \n, and \r\n
+    println("""\r?\n""".r.findAllIn(multilineString).toList)      //one or none of \r and one of \n so matches \r\n and \n
+    println("""\R""".r.findAllIn(multilineString).toList)
+  }
+
+  "find end of input vs. end of line" should "work" in {
+
+    println("""-------------"""+"""^(.*?)$""".r.findAllIn("""auser1 home1b""").toList)    //match start/end of INPUT
+
+    println("""-------------"""+"""(?m)^(.*?)$""".r.findAllIn(        //multiline mode - makes ^ and $ apply to start/end of LINE
+      """auser1 home1b
+        |auser1 home1b""".stripMargin).toList)
+
+
+
+  }
+
+  "replacing empty lines" should "work" in {
+
+
+  }
+}
